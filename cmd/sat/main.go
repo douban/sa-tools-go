29aa2c66fc006cc451173d2fd4d844a4cc7f1714
everpcpc <git@everpcpc.com>
2021-11-29 12:24:55 +0800

    main: init command sat

---
+++ cmd/sat/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var (
+	logger = logrus.New()
+	ctx    = context.Background()
+
+	flagConfig string
+)
+
+func main() {
+	var (
+		flagVerbose bool
+		flagQuiet   bool
+	)
+
+	subCommands := []*cobra.Command{}
+
+	rootCmd := &cobra.Command{
+		Use: "sat",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			logger.SetOutput(os.Stdout)
+			if flagVerbose {
+				logger.SetLevel(logrus.DebugLevel)
+			} else if flagQuiet {
+				logger.SetLevel(logrus.WarnLevel)
+			} else {
+				logger.SetLevel(logrus.InfoLevel)
+			}
+		},
+	}
+	rootCmd.PersistentFlags().BoolVarP(&flagVerbose, "verbose", "v", false, "log debug")
+	rootCmd.PersistentFlags().BoolVarP(&flagQuiet, "quiet", "q", false, "log warning")
+
+	rootCmd.AddCommand(subCommands...)
+	rootCmd.Execute()
+}
